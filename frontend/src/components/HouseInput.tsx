1ebd0c58234b4c033b75998ece799b84a0d09621
Gtsind <tsindarisg@gmail.com>
2025-06-18 00:35:49 +0300

    http request wip

--- frontend/src/components/HouseInput.tsx
+++ frontend/src/components/HouseInput.tsx
@@ -1,10 +1,38 @@
+import { useState, useEffect } from "react";
+
 export default function HouseInput() {
+  const [userInput, setUserInput] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUserInput(event.target.value);
+  }
+
+  useEffect(() => {
+    async function fetchHouses() {
+      if (!userInput.trim()) return;
+
+      try {
+        const response = await fetch(
+          `http://localhost:3000/houses?name=${userInput}`
+        );
+        const resData = await response.json();
+        console.log(resData.reply.founder);
+      } catch (error) {
+        console.log("Error >>", error);
+      }
+    }
+
+    fetchHouses();
+  }, [userInput]);
+
   return (
     <div className="flex justify-center mt-20">
       <input
         className="border border-gray-300 p-2 rounded-md"
         type="text"
         placeholder="Search houses"
+        value={userInput}
+        onChange={handleChange}
       />
     </div>
   );
