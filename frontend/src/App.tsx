a3d218c76ea912894a5efe690a183ac3a0c2b5af
Gtsind <tsindarisg@gmail.com>
2025-06-20 13:52:01 +0300

    feat: add loading spinner and fake delay

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,9 @@ import HouseContainer from "./components/HouseContainer.tsx";
 import HouseInput from "./components/HouseInput.tsx";
 import type { House } from "./shared/types/house.types.ts";
 import type { HouseApiResponse } from "./shared/interfaces/house-api-response.interfaces.ts";
+import { sleep } from "./shared/util/sleep.ts";
+
+const FAKE_DELAY = 2000;
 
 function mapResponseToHouses(response: HouseApiResponse[]): House[] {
   return response.map((house) => ({
@@ -17,6 +20,7 @@ function mapResponseToHouses(response: HouseApiResponse[]): House[] {
 function App() {
   const [userInput, setUserInput] = useState<string>("");
   const [houses, setHouses] = useState<House[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     setUserInput(event.target.value);
@@ -24,28 +28,35 @@ function App() {
 
   useEffect(() => {
     async function fetchHouses() {
+      setIsLoading(true);
       if (!userInput.trim()) {
         // If input is empty, fetch all houses (on load app, or on input clear)
         try {
           const response = await fetch("http://localhost:3000/houses");
+          await sleep(FAKE_DELAY);
           const resData = await response.json();
           const houseArray = mapResponseToHouses(resData.reply);
           setHouses(houseArray);
+          setIsLoading(false);
         } catch (error) {
           console.log("Error >>", error);
         }
+
         return;
       }
 
       try {
+        setIsLoading(true);
         const response = await fetch(
           `http://localhost:3000/houses?name=${userInput}`
         );
+        await sleep(FAKE_DELAY);
         const resData = await response.json();
         console.log(resData.reply);
         const house = mapResponseToHouses([resData.reply]);
         // console.log("HOUSES >>>", house);
         setHouses(house);
+        setIsLoading(false);
       } catch (error) {
         console.log("Error >>", error);
       }
@@ -57,7 +68,7 @@ function App() {
   return (
     <>
       <HouseInput userInput={userInput} onChangeInput={handleChange} />
-      <HouseContainer houses={houses} />
+      <HouseContainer houses={houses} isLoading={isLoading} />
     </>
   );
 }
--- frontend/src/components/HouseContainer.tsx
+++ frontend/src/components/HouseContainer.tsx
@@ -1,16 +1,25 @@
 import HouseCard from "./HouseCard.tsx";
+import { SpinnerCircular } from "spinners-react";
 import type { House } from "../shared/types/house.types.ts";
 
 type HouseContainerProps = {
   houses: House[];
+  isLoading: boolean;
 };
 
-export default function HouseContainer({ houses }: HouseContainerProps) {
+export default function HouseContainer({
+  houses,
+  isLoading,
+}: HouseContainerProps) {
   return (
     <section className="flex flex-col justify-center items-center mt-2">
-      {houses.map((house) => (
-        <HouseCard key={house.name} house={house} />
-      ))}
+      {isLoading && (
+        <div className="flex h-40">
+          <SpinnerCircular color="black" />
+        </div>
+      )}
+      {!isLoading &&
+        houses.map((house) => <HouseCard key={house.name} house={house} />)}
     </section>
   );
 }
